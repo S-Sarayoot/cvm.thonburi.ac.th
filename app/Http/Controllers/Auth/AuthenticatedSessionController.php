f84eee610ed795eb73847dbce85c640172e06004
S.Sarayoot <sarayoot_ball12@hotmail.com>
2025-09-24 03:03:26 +0700

    อัพเดต Authen

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,7 +34,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->intended(route('admin', absolute: false));
         } else {
             // investors หรือ role อื่น
-            return redirect()->intended(route('dashboard', absolute: false));
+            return redirect()->intended(route('user', absolute: false));
         }
 
         //return redirect()->intended(route('dashboard', absolute: false));
