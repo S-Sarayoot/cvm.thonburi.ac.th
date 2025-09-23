c0fd06ddeb3208630f6d2cad4a47c325b0ed0500
S.Sarayoot <sarayoot_ball12@hotmail.com>
2025-09-24 01:08:57 +0700

    update Migrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,37 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
+            'username' => 'admin',
             'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'reset_password_token' => null,
+            'reset_password_sent_at' => null,
+            'is_admin' => 1,
+            'date_of_birth' => '1990-01-01',
+            'phone' => '0812345678',
+            'address' => '123 Main St',
+            'province' => 'Bangkok',
+            'zipcode' => '10100',
+            'department_name' => 'IT',
+            'is_approved' => 1,
+        ]);
+
+        // User ปกติ
+        User::factory()->create([
+            'name' => 'Normal User',
+            'username' => 'user01',
+            'email' => 'user@example.com',
+            'password' => bcrypt('password'),
+            'reset_password_token' => null,
+            'reset_password_sent_at' => null,
+            'is_admin' => 0,
+            'date_of_birth' => '1995-05-05',
+            'phone' => '0898765432',
+            'address' => '456 Second St',
+            'province' => 'Chiang Mai',
+            'zipcode' => '50000',
+            'department_name' => 'HR',
+            'is_approved' => 1,
         ]);
     }
 }
