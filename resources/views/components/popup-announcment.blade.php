a08a122a36bc079d73970c55d54b1cb1ab9d459c
namhom <namhom@test.com>
2025-09-25 22:33:38 +0700

    ✨ feat(layout): welcome

--- resources/views/components/popup-announcment.blade.php
+++ resources/views/components/popup-announcment.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ open: open }" x-show="open"
     class="fixed inset-0 bg-black/50 flex items-center justify-center z-50" x-cloak>
 
-    <div class="shadow-2xl max-w-xl text-center relative transition transform scale-100">
+    <div class="max-w-xl text-center relative">
         <button @click="open = false" class="absolute top-2 bg-white/40 px-2 rounded-md right-2 text-white hover:text-red-500 text-xl font-bold">
             ✕
         </button>
