a6641e533446d4772d89ac2702b286b48cbadc5b
namhom <namhom@test.com>
2025-09-27 00:51:03 +0700

    search

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,6 +44,26 @@
         </div>       
         <div class="flex flex-col  bg-white rounded-br-lg shadow-md hover:shadow-xl fixed " x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 transform -translate-y-2" x-transition:enter-end="opacity-100 transform translate-y-0" x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 transform translate-y-0" x-transition:leave-end="opacity-0 transform -translate-y-2">
             <ul>
+                <li class="px-12 py-4 cursor-pointer flex items-center md:hidden">
+                        <form class="max-w-sm min-w-[200px]">
+                            <div class="relative flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="absolute w-5 h-5 top-1.5 left-3 text-slate-600">
+                                    <path fill-rule="evenodd"
+                                        d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <input
+                                    class="ml-2 w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-7 py-1 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-md focus:shadow"
+                                    placeholder="serch" />
+                                <button
+                                    class="rounded-md bg-[#8a438f] py-1 px-2 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg hover:bg-purple-700 ml-2"
+                                    type="submit">
+                                    Search
+                                </button>
+                            </div>
+                        </form>
+                </li>
                 <li class="px-12 text-center py-4 hover:bg-[#8a438f]/20 cursor-pointer text-sm flex items-center" @click="{{ route('welcome') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" class="size-6 mr-1" viewBox="0 0 24 24">
                         <path
